38647e6ecccda28d3b7732f7d32a8e5892b2926a
Manuel Barros Reyes <manuca@gmail.com>
2013-03-29 20:14:30 -0300

    Rspec

--- lib/game.rb
+++ lib/game.rb
@@ -1,33 +1,38 @@
+# Game controller
 class Game
-  def initialize
+  def initialize(screen)
     @current_piece = nil
     @over = false
     @brick_layout = BrickLayout.new
-  end
-
-  def over?
-    @over
+    @screen = screen
+    @screen.layout = @brick_layout
   end
 
   def play
     iterate while !over?
   end
 
-  def next_piece
-    @current_piece = Piece.next
-    @current_piece.brick_layout = @brick_layout
-  end
-
   def iterate
     next_piece if @current_piece.nil?
 
-    if @current_piece.hit_bottom?
+    if @brick_layout.hit_bottom?(@current_piece)
       @brick_layout.fix_piece(@current_piece)
       @current_piece = nil
     else
       @current_piece.advance_down
-      @brick_layout.update(@current_piece)
+      @brick_layout.current_piece(@current_piece)
       @over = true if @brick_layout.hit_top?
     end
+
+    @screen.redraw
+  end
+
+  def over?
+    @over
+  end
+
+  def next_piece
+    @current_piece = Piece.next
+    @current_piece.brick_layout = @brick_layout
   end
 end
