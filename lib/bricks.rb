a9318ba3504d626fe90c7b74be294738202d432d
Manuel Barros Reyes <manuca@gmail.com>
2013-04-01 17:14:15 -0300

    Some more improvements

--- lib/bricks.rb
+++ lib/bricks.rb
@@ -1,2 +1,8 @@
 class Bricks
+  def initialize(width, height)
+    @width, @height = width, height
+  end
+
+  def occupied?(x, y)
+  end
 end
--- lib/piece.rb
+++ lib/piece.rb
@@ -1,13 +1,39 @@
+# Tetrimino
 class Piece 
   attr_accessor :bricks
 
   def initialize(type)
   end
 
-  # def self.next
-  #   new
-  # end
+  def position(x, y)
+    @x, @y = x, y
+  end
+  
+  # Coordinates of parts relative to center of gravity
+  def parts
+    [[-1, 0], [0, 0], [1, 0], [2, 0]]
+  end
 
-  # def advance_down
-  # end
+  def down_possible?
+    # For each part of the tetrimino 
+    # calculate it coordinates
+    # Check neighbour below if empty
+    # If all neighbours are empty
+    # return true
+    # else return false
+    
+    possible = true
+
+    parts.each do |p|
+      x = @x + p[0] 
+      y = @y + p[1] - 1
+
+      if (y == 0) || bricks.occupied?(x, y)
+        possible = false 
+        break
+      end
+    end
+
+    possible
+  end
 end
